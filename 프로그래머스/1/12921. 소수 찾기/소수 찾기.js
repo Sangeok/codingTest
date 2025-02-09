2f79ccee5cba2f098b34914e588ead9e93fd2a07
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-02-09 19:58:32 +0900

    [level 1] Title: 소수 찾기, Time: 140.82 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,33 @@
+function solution(n) {
+    let result = [];
+    for(let i = 2; i<=n; i++) {
+        if(isPrime(i)) {
+            result.push(i);
+        }
+    }
+    
+    return result.length;
+}
+
+function isPrime(n) {
+    if(n<2) {
+        return false;
+    }
+    
+    if(n === 2) {
+        return true;
+    }
+    
+    if(n % 2 === 0) {
+        return false;
+    }
+    
+    for(let i = 3; i<=Math.sqrt(n); i+=2) {
+        if(n % i === 0) {
+            return false;
+            break;
+        }
+    }
+    
+    return true;
+}
