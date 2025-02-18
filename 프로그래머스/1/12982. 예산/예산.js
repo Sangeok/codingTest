70d620d8f9e220dd5a2a26e2b58abd6ef55a0eb5
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-02-18 17:49:05 +0900

    [level 1] Title: 예산, Time: 0.16 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,17 @@
+function solution(d, budget) {
+    let count = 0;
+    
+    d.sort((a,b) => a - b);
+    
+    for(let i = 0; i<d.length; i++) {
+        if(budget - d[i] >= 0) {
+            budget = budget - d[i];
+            count++;
+        }
+        else {
+            break;
+        }
+    }
+    
+    return count;
+}
