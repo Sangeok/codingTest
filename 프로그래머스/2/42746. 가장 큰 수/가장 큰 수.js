a29237506f7582daf733b1e8753712c8bd6b3d59
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-02-22 02:46:14 +0900

    [level 2] Title: 가장 큰 수, Time: 169.54 ms, Memory: 44.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,16 @@
+function solution(numbers) {
+    // 숫자 배열을 문자열로 변환
+    const strNumbers = numbers.map(num => num.toString());
+    
+    // 두 수를 연결했을 때 더 큰 수가 되는 순서로 정렬
+    // (b+a) - (a+b)는 양수면 b가 앞에, 음수면 a가 앞에 오도록 함
+    strNumbers.sort((a, b) => {
+        return (b + a) - (a + b);
+    });
+    
+    // 정렬된 배열을 하나의 문자열로 합침
+    const answer = strNumbers.join('');
+    
+    // 결과가 '0'으로 시작하면 (모든 요소가 0인 경우) '0' 반환
+    return answer[0] === '0' ? '0' : answer;
+}
