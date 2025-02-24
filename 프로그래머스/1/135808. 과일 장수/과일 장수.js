cfbd6ea086e289a5c43e69183b04efd503cf01e4
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-02-24 16:36:53 +0900

    [level 1] Title: 과일 장수, Time: 265.24 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,18 +1,10 @@
 function solution(k, m, score) {
-    let sum = 0;
-    
+    let result = 0;
     score.sort((a,b) => b-a);
-    let newArr = chunkArr(score,m);
     
-    for(let i = 0; i<newArr.length; i++) {
-        let min = Math.min(...newArr[i]);
-        sum = sum + (min *m);
+    for(let i = m-1; i<score.length; i = i + m) {
+        result = result + m * score[i];
     }
-    return sum;
     
-}
-
-function chunkArr(array,size) {
-    return Array.from({length : Math.floor(array.length/size)}, (_,i) => array.slice(i*size, (i+1)*size));
-    
+    return result;
 }
