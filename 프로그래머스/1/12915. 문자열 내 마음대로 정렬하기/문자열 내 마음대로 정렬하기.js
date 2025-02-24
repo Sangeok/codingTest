29440cc3b15ae3f32698364df96f6b03bc839813
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-02-24 16:14:08 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 37.42 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,27 +1,58 @@
+// function solution(strings, n) {
+//     let popStringArr = [];
+//     let result = [];
+    
+//     for(let i = 0; i<strings.length; i++) {
+//         let popString = strings[i].split('')[n];
+//         const obj = {
+//             char : popString,
+//             index : i,
+//             str : strings[i]
+//         }
+//         popStringArr.push(obj);
+//     }
+    
+//     popStringArr.sort((a,b) => {
+//         if(a.char === b.char) {
+//             return a.str.localeCompare(b.str);
+//         }
+//         else {
+//             return a.char.localeCompare(b.char);
+//         }
+//     });
+    
+//     popStringArr.map((item) => result.push(item.str));
+    
+//     return result;
+// }
+
 function solution(strings, n) {
-    let popStringArr = [];
-    let result = [];
+    let charObjArr = [];
     
     for(let i = 0; i<strings.length; i++) {
-        let popString = strings[i].split('')[n];
-        const obj = {
-            char : popString,
-            index : i,
-            str : strings[i]
+        let strArr = strings[i].split('');
+        let char = strArr[n];
+        
+        let charObj = {
+            id : i,
+            str : strings[i],
+            char,
         }
-        popStringArr.push(obj);
+        charObjArr.push(charObj);
     }
-    
-    popStringArr.sort((a,b) => {
+
+    charObjArr.sort((a,b) => {
         if(a.char === b.char) {
             return a.str.localeCompare(b.str);
         }
         else {
             return a.char.localeCompare(b.char);
         }
-    });
+    })
     
-    popStringArr.map((item) => result.push(item.str));
+    return charObjArr.map((item) => item.str);
     
-    return result;
+    
+    
+    
 }
