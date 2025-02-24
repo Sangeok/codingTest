ae8aa44628a9ccee450cc495bf86fd661d35c820
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-02-24 16:15:51 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 16.98 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,31 +1,3 @@
-// function solution(strings, n) {
-//     let popStringArr = [];
-//     let result = [];
-    
-//     for(let i = 0; i<strings.length; i++) {
-//         let popString = strings[i].split('')[n];
-//         const obj = {
-//             char : popString,
-//             index : i,
-//             str : strings[i]
-//         }
-//         popStringArr.push(obj);
-//     }
-    
-//     popStringArr.sort((a,b) => {
-//         if(a.char === b.char) {
-//             return a.str.localeCompare(b.str);
-//         }
-//         else {
-//             return a.char.localeCompare(b.char);
-//         }
-//     });
-    
-//     popStringArr.map((item) => result.push(item.str));
-    
-//     return result;
-// }
-
 function solution(strings, n) {
     let charObjArr = [];
     
@@ -51,8 +23,4 @@ function solution(strings, n) {
     })
     
     return charObjArr.map((item) => item.str);
-    
-    
-    
-    
 }
