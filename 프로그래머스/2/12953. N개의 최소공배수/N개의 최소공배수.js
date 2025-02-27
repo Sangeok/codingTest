45d7f9239f7d3fd820a63d69758076f2c1b4360e
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-02-27 15:02:31 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.12 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,24 @@
+function solution(arr) {
+    let result = arr[0];
+    for(let i = 1; i<arr.length; i++) {
+        result = getLCM(result, arr[i]);
+    }
+    
+    return result;
+}
+
+// 최소공배수
+function getLCM(a,b) {
+    return (a * b) / getGCD(a,b);
+}
+
+// 최대공약수
+function getGCD(a,b) {
+    while(b>0) {
+        let temp = a % b;
+        a = b;
+        b = temp;
+    }
+    
+    return a;
+}
