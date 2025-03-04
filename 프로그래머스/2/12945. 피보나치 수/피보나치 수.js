d3641cd6f6041f74055d2048dac8b29d086c4756
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-03-04 23:06:55 +0900

    [level 2] Title: 피보나치 수, Time: 1.58 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,13 +1,11 @@
 function solution(n) {
-    if(n === 0) return 0;
-    if(n === 1) return 1;
+    if(n === 2) return 1;
     
-    let fibo1 = 0;
+    let fibo1 = 1;
     let fibo2 = 1;
     
-    for(let i = 2; i<=n; i++) {
+    for(let i = 3; i<=n; i++) {
         let result = (fibo1 + fibo2) % 1234567;
-        
         fibo1 = fibo2;
         fibo2 = result;
     }
