b6fcd09df31a39060d1a5dd39e02095aa7f6963f
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-03-04 23:03:05 +0900

    [level 2] Title: 숫자의 표현, Time: 0.49 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,12 +1,24 @@
+// function solution(n) {
+//     let count = 0;
+    
+//     for(let k = 1; k<=n; k++) {
+//         let number = n - (k * (k-1) / 2);
+        
+//         if(number / k >= 1 && number % k === 0) { 
+//             count++;
+//         }
+//     }
+    
+//     return count;
+// }
+
 function solution(n) {
     let count = 0;
     
     for(let k = 1; k<=n; k++) {
-        let number = n - (k * (k-1) / 2);
-        
-        if(number / k >= 1 && number % k === 0) { 
+        if(2*n - Math.pow(k,2) + k > 0 && (2*n - Math.pow(k,2) + k) % (2*k) === 0) {
             count++;
-        }
+        }    
     }
     
     return count;
