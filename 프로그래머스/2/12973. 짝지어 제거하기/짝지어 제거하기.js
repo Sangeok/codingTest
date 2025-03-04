08c234e22e13a47b4c55a6efd2858043a71c2bca
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-03-04 23:59:01 +0900

    [level 2] Title: 짝지어 제거하기, Time: 52.14 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,15 +1,30 @@
-function solution(s)
-{
+// function solution(s)
+// {
+//     let stack = [];
+    
+//     for(let i = 0; i<s.length; i++) {
+//         let currentChar = s[i];
+        
+//         if(stack[stack.length -1] === currentChar) {
+//             stack.pop();
+//         }
+//         else {
+//             stack.push(currentChar);
+//         }
+//     }
+    
+//     return stack.length === 0 ? 1 : 0;
+// }
+
+function solution(s) {
     let stack = [];
     
     for(let i = 0; i<s.length; i++) {
-        let currentChar = s[i];
-        
-        if(stack[stack.length -1] === currentChar) {
+        if(stack[stack.length-1] === s[i]) {
             stack.pop();
         }
         else {
-            stack.push(currentChar);
+            stack.push(s[i]);
         }
     }
     
