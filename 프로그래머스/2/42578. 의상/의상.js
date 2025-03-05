c85c48fd6f78989e2b2ebebdd84f6ca27b30c1cf
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-03-05 22:58:42 +0900

    [level 2] Title: 의상, Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,16 +1,16 @@
 function solution(clothes) {
     let result = 1;
-    let clothObj = {};
+    let obj = {};
     
-    for(const [k, v] of clothes) {
-        clothObj[v] = (clothObj[v] || 0) + 1;
+    for(const [v,k] of clothes) {
+        obj[k] = (obj[k] || 0) + 1;
     }
     
-    for(let i of Object.values(clothObj)) {
-        result = result * (i+1);
+    for(let value of Object.values(obj)) {
+        result = result * (value+1);
     }
     
-    return result-1;
+    return result - 1;
     
 }
 
