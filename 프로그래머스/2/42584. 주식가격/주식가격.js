8cb5d3b95ff70913ae224481f6e3791039c32d36
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-03-06 23:58:32 +0900

    [level 2] Title: 주식가격, Time: 28.70 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,19 +1,39 @@
+// function solution(prices) {
+//     let stack = [];
+//     let answer = new Array(prices.length).fill(0);
+    
+//     for(let i = 0; i<prices.length; i++) {
+//         while(stack.length && prices[stack[stack.length - 1]] > prices[i]) {
+//             const top = stack.pop();
+//             answer[top] = i-top;
+//         }   
+//         stack.push(i);
+//     }
+    
+//     while(stack.length) {
+//         const pop = stack.pop();
+//         answer[pop] = prices.length - 1 - pop;
+//     }
+    
+//     return answer;
+// }
+
 function solution(prices) {
     let stack = [];
-    let answer = new Array(prices.length).fill(0);
+    let result = new Array(prices.length).fill(0);
     
     for(let i = 0; i<prices.length; i++) {
-        while(stack.length && prices[stack[stack.length - 1]] > prices[i]) {
+        while(stack.length !== 0 && prices[stack[stack.length-1]] > prices[i]) {
             const top = stack.pop();
-            answer[top] = i-top;
-        }   
+            result[top] = i-top;
+        }
         stack.push(i);
     }
     
-    while(stack.length) {
-        const pop = stack.pop();
-        answer[pop] = prices.length - 1 - pop;
+    while(stack.length > 0) {
+        let pop = stack.pop();
+        result[pop] = prices.length-1-pop;
     }
     
-    return answer;
+    return result;
 }
