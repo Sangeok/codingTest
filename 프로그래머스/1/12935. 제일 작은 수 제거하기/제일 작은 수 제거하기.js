54cde22148f1bb6d42a22d8d3b6794d09a25ad16
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-06-10 13:46:53 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.63 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,6 +1,5 @@
 function solution(arr) {
-    if(arr.length <= 1) return [-1];
-    
+    if(arr.length === 1) return [-1];
     
     let min = Math.min(...arr);
     return arr.filter((item) => item !== min);
