22ec3622fa9b8f0e69c5deeba13550978dd1ffef
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-06-15 04:27:55 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,20 +1,15 @@
 function solution(s) {
-    let newArr = s.split(' ');
-    let result = ""
-    for(let i = 0 ;i<newArr.length; i++) {
-        let str = "";
-        for(let j = 0; j<[...newArr[i]].length; j++) {
-            if(j % 2 === 0) {
-                str = str + newArr[i][j].toUpperCase();
+    s = s.split(' ');
+    let str = "";
+    for(let t of s) {
+        for(let [index, char] of [...t].entries()) {
+            if(index % 2 === 0) {
+                str = str + char.toUpperCase();
             }
-            else {
-                str = str + newArr[i][j].toLowerCase();
-            }
+            else str = str + char.toLowerCase();
         }
-        if(i !== newArr.length-1) result = result + str + " "
-        else result = result + str
+        str = str + " ";
     }
-    console.log(result)
-    return result;
     
+    return str.slice(0,-1);
 }
