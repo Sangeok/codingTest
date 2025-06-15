f08823f4da5cfe5f5d27f53ee276c8ecc35de3db
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-06-15 12:34:30 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 6.62 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,23 +1,15 @@
 function solution(s) {
+    let obj = {};
     let result = [];
-    let alphabetMap = new Map();
     
-    for (let i = 65; i <= 90; i++) {
-        let alphabet = String.fromCharCode(i).toLowerCase();
-        alphabetMap.set(alphabet, -1);
-    }
-
-    let sArr = s.split('');
-    for(let i = 0; i<sArr.length; i++) {
-        let currentValue = alphabetMap.get(sArr[i]);
-        if(currentValue === -1) {
+    for(let [index, char] of [...s].entries()) {
+        if(obj[char] !== undefined) {
+            let num = index - obj[char];
+            result.push(num);
+            obj[char] = index;
+        } else {
             result.push(-1);
-            alphabetMap.set(sArr[i], i);
-        }
-        else {
-            let nowNum = i - currentValue;
-            result.push(nowNum);
-            alphabetMap.set(sArr[i], i);
+            obj[char] = index;
         }
     }
     
