465c72cf934b3b325e15635239749604fe6dc1fb
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-06-22 00:02:15 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,14 +1,10 @@
 function solution(array, commands) {
     let result = [];
-
-    for(let i = 0; i<commands.length; i++) {
-        let startIndex = commands[i][0]-1;
-        let endIndex = commands[i][1];
-        let findIndex = commands[i][2]-1;
-        
-        let newArr = array.slice(startIndex,endIndex).sort((a,b) => a-b);
-        
-        result.push(newArr[findIndex]);
+    for(let t = 0; t<commands.length; t++) {
+        let [i,j,k] = commands[t];
+        let newArr = array.slice(i-1,j).sort((a,b) => a-b);
+        let findNum = newArr[k-1];
+        result.push(findNum);
     }
     
     return result;
