856cf4087a29db0b7dced487242c3bf529731f24
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-06-22 21:00:30 +0900

    [level 1] Title: 명예의 전당 (1), Time: 23.79 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -3,27 +3,14 @@ function solution(k, score) {
     let goodSinger = [];
     
     for(let i = 0; i<score.length; i++) {
-        if(i <= k-1) {
-            goodSinger.push(score[i]);
-            let minScore = Math.min(...goodSinger);
-            result.push(minScore);
-        } else {
-            let minScore = Math.min(...goodSinger);
-            let currentScore = score[i];
-            
-            if(currentScore > minScore) {
-                // 오늘 가수가 명예의 전당에 있던 가수보다 점수가 높은 경우
-                let minIndex = goodSinger.indexOf(minScore);
-                goodSinger[minIndex] = currentScore;
-                
-                minScore = Math.min(...goodSinger);
-                result.push(minScore);
-            }
-            else {
-                // 기존 명예의 전당에 있던 가수의 점수가 더 높은 경우
-                result.push(minScore);
-            }
-        }
+        let nowScore = score[i];
+        
+        goodSinger.push(nowScore);
+        goodSinger.sort((a,b) => b-a);
+        
+        if(goodSinger.length > k) goodSinger.pop();
+        
+        result.push(goodSinger[goodSinger.length-1]);
     }
     
     return result;
