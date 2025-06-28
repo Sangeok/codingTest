3c82503610d4dedaa3427ab86ec42b01785cc3d9
Sangeok <103489700+Sangeok@users.noreply.github.com>
2025-06-28 22:33:24 +0900

    [level 1] Title: 소수 만들기, Time: 7.30 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,34 +1,25 @@
 function solution(nums) {
     let count = 0;
     
-    for(let i = 0; i<nums.length; i++) {
-        for(let j = i+1; j<nums.length; j++) {
+    for(let i = 0; i<nums.length-2; i++) {
+        for(let j = i+1; j<nums.length-1; j++) {
             for(let k = j+1; k<nums.length; k++) {
-                if(isPrime(nums[i]+nums[j]+nums[k])) {
-                    count++;        
-                }
+                let sum = nums[i] + nums[j] + nums[k];
+                if(isPrime(sum)) count++;
             }
         }
     }
-    
     return count;
-    
-    
 }
 
 function isPrime(n) {
-    if(n < 2) return false;
-    
+    if(n<2) return false;
     if(n === 2) return true;
-    
     if(n % 2 === 0) return false;
     
     for(let i = 3; i<=Math.sqrt(n); i++) {
-        if(n%i === 0) {
-            return false;
-        }
+        if(n % i === 0) return false;
     }
     
     return true;
-    
 }
