9c23c9c070975b463c9bff818a266c461a13030b
Sangeok <hamsoo159@naver.com>
2025-10-22 00:47:47 +0900

    [level 1] Title: 약수의 합, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,11 +1,7 @@
 function solution(n) {
-    let factors = getFactors(n);
-    let sum = 0;
-    for(let n of factors) {
-        sum = sum + n;
-    }
-
-    return sum;
+    const factors = getFactors(n);
+    
+    return factors.reduce((acc,cur) => acc + cur, 0);
 }
 
 function getFactors(n) {
@@ -14,6 +10,7 @@ function getFactors(n) {
     for(let i = 1; i<=Math.sqrt(n); i++) {
         if(n % i === 0) {
             result.push(i);
+            
             if(i !== n/i) {
                 result.push(n/i);
             }
