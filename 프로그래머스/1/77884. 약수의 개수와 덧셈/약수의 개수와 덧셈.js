cfb2c13c8ec6cfb50d6ffdfe5f05092a186289cd
Sangeok <hamsoo159@naver.com>
2025-10-22 18:51:27 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 20.65 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,24 +1,22 @@
 function solution(left, right) {
     let sum = 0;
     for(let i = left; i<=right; i++) {
-        let FactorNum = getFactors(i);
-        if(FactorNum % 2 === 0) {
+        if(getFactorsLength(i) % 2 === 0) {
             sum = sum + i;
+        } else {
+            sum = sum - i;
         }
-        else sum = sum - i;
     }
     
     return sum;
 }
 
-// 약수 구하기
-function getFactors(n) {
+function getFactorsLength(n) {
     let result = [];
     
     for(let i = 1; i<=Math.sqrt(n); i++) {
         if(n % i === 0) {
             result.push(i);
-            
             if(i !== n/i) {
                 result.push(n/i);
             }
