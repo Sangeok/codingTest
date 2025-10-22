86271e59bc974dc4e1a8f0a0d8d55dbc098e3369
Sangeok <hamsoo159@naver.com>
2025-10-22 20:32:08 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,19 +1,19 @@
 function solution(n, m) {
-    return [getGCD(n,m), getLCM(n,m)];
+    return [getGCD(n,m), getLCM(n,m)]
 }
 
 // 최소공배수
-function getLCM(n, m) {
-    return Math.abs(n*m) / getGCD(n,m);
+function getLCM(a,b) {
+    return Math.abs(a*b) / getGCD(a,b);
 }
 
 // 최대공약수
-function getGCD(n, m) {
-    while(m != 0) {
-        let temp = m;
-        m = n % m;
-        n = temp;
+function getGCD(a,b) {
+    while(b != 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
     }
     
-    return n;
+    return a;
 }
