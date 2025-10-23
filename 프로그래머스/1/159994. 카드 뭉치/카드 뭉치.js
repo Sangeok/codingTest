55750c0a3fdee9d3ff768c65a311af107d0b5196
Sangeok <hamsoo159@naver.com>
2025-10-24 03:12:17 +0900

    [level 1] Title: 카드 뭉치, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,17 +1,19 @@
 function solution(cards1, cards2, goal) {
-    for(let i = 0; i<goal.length; i++) {
-        let currentStr = goal[i];
+    let count1 = 0;
+    let count2 = 0;
+    
+    for(let i of goal) {
+        let cards1_word = cards1[count1];
+        let cards2_word = cards2[count2];
         
-        let card1Str = cards1.shift();
-        let card2Str = cards2.shift();
-        
-        if(currentStr === card1Str) {
-            cards2.unshift(card2Str);
-        } else if (currentStr === card2Str){
-            cards1.unshift(card1Str);
-        } else {
-            return "No";
+        if(cards1_word === i) {
+            count1++;
+            continue;
+        } else if(cards2_word === i) {
+            count2++;
+            continue;
         }
+        else return "No"
     }
     
     return "Yes";
