62bd3404335128d3794ed027f5f5919e3fa8ae60
Sangeok <hamsoo159@naver.com>
2025-10-24 02:21:21 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,10 +1,14 @@
 function solution(array, commands) {
     let result = [];
-    for(let t = 0; t<commands.length; t++) {
-        let [i,j,k] = commands[t];
-        let newArr = array.slice(i-1,j).sort((a,b) => a-b);
-        let findNum = newArr[k-1];
-        result.push(findNum);
+    
+    for(let i = 0; i<commands.length; i++) {
+        const sliceStart = commands[i][0];
+        const sliceEnd = commands[i][1];
+        
+        const num = array.slice(sliceStart-1,sliceEnd);
+        num.sort((a,b) => a-b);
+        
+        result.push(num[commands[i][2]-1]);
     }
     
     return result;
