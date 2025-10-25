2dff9d355237eb4d893ba84a7deed38d88cb6811
Sangeok <hamsoo159@naver.com>
2025-10-26 02:51:42 +0900

    [level 1] Title: 과일 장수, Time: 224.02 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,13 +1,13 @@
 function solution(k, m, score) {
+    let sum = 0;
     score.sort((a,b) => b-a);
     
-    let sum = 0;
     for(let i = 0; i<score.length; i = i+m) {
-        let newArr = score.slice(i,i+m);
-        if(newArr.length !== m) continue;
-    
-        let p = Math.min(...newArr);
-        sum = sum + p * newArr.length;
+        if(i+m > score.length) break;
+        let newArr = score.slice(i, i+m);
+        
+        const minNum = Math.min(...newArr);
+        sum = sum + newArr.length * minNum
     }
     
     return sum;
