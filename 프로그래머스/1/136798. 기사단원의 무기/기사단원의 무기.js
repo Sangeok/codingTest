03cb4896df10f360888f03e498e103117da5a0f7
Sangeok <hamsoo159@naver.com>
2025-10-26 00:23:00 +0900

    [level 1] Title: 기사단원의 무기, Time: 157.71 ms, Memory: 42.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,25 +1,23 @@
 function solution(number, limit, power) {
-    let obj = {};
-    let result = 0;
+    let member = [0];
     
     for(let i = 1; i<=number; i++) {
-        let attackNum = getDivisorNum(i);
-        if(attackNum > limit) {
-            obj[i] = power;
-        } else {
-            obj[i] = attackNum;
+        let factorsLength = getFactors(i);
+        if(factorsLength > limit) {
+            member.push(power);
+            continue;
         }
+        member.push(factorsLength);
     }
     
-    for(let num of Object.values(obj)) {
-        result = result + num;
-    }
+    const sum = member.reduce((cur, acc) => cur + acc, 0);
     
-    return result;
+    return sum;
 }
 
-function getDivisorNum(n) {
+function getFactors(n) {
     let result = [];
+    
     for(let i = 1; i<=Math.sqrt(n); i++) {
         if(n % i === 0) {
             result.push(i);
