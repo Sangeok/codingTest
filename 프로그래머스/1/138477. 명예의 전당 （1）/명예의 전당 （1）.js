1017f99b1ee92d41b60758ce4ff2616c4c5693c6
Sangeok <hamsoo159@naver.com>
2025-10-29 18:07:06 +0900

    [level 1] Title: 명예의 전당 (1), Time: 5.87 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -2,26 +2,16 @@ function solution(k, score) {
     let legend = [];
     let result = [];
     
-    for(let i = 0; i<score.length; i++) {
-        const nowSinger = score[i];
-        const beforeLegend = legend[0] || 0;
+    for(let i of score) {
+        legend.push(i);
         
-        if(i < k) {
-            legend.push(nowSinger);
-            
-            const minNum = Math.min(...legend);
-            result.push(minNum);
-        }
-        else {
-            legend.push(nowSinger);
-            
-            legend.sort((a,b) => a - b);
-            
-            const newArr = [...legend].splice(legend.length-k,legend.length);
-            const minNum = Math.min(...newArr);
-            result.push(minNum);
+        legend.sort((a,b) => b-a);
+        
+        if(legend.length > k) {
+            legend.pop();
         }
         
+        result.push(legend[legend.length-1]);
     }
     
     return result;
