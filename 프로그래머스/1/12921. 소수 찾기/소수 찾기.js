28cf4a8930f354524f035297db3407ed4ffec715
Sangeok <hamsoo159@naver.com>
2025-10-30 20:47:17 +0900

    [level 1] Title: 소수 찾기, Time: 54.50 ms, Memory: 45 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,13 +1,16 @@
 function solution(n) {
     let isPrime = new Array(n+1).fill(true);
+    
     isPrime[0] = false;
     isPrime[1] = false;
     
     for(let i = 2; i*i<=n; i++) {
-        for(let j = i * i; j<=n; j = j + i) {
-            isPrime[j] = false;
+        if(isPrime[i]) {
+            for(let j = i*i; j<=n; j = j + i) {
+                isPrime[j] = false;
+            }
         }
     }
     
-    return isPrime.filter((x) => x === true).length;
+    return isPrime.filter((item) => item === true).length;
 }
