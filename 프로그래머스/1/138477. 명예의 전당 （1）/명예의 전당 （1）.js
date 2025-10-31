3dc9fb6eda81df21c3b77b1c131da360f0df18d6
Sangeok <hamsoo159@naver.com>
2025-10-31 20:22:35 +0900

    [level 1] Title: 명예의 전당 (1), Time: 3.85 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -6,14 +6,14 @@ function solution(k, score) {
         legend.push(i);
         
         legend.sort((a,b) => b-a);
+
         
-        if(legend.length > k) {
-            legend.pop();
+        if(legend.length <= k) {
             result.push(legend[legend.length-1]);
         } else {
+            legend.pop();
             result.push(legend[legend.length-1]);
         }
-        
     }
     
     return result;
