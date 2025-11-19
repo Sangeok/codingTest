48a7e843610d2d5b0d0b1c61ea087f308661a1bf
Sangeok <hamsoo159@naver.com>
2025-11-19 22:12:46 +0900

    [level 1] Title: 숫자 짝꿍, Time: 199.65 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -4,28 +4,26 @@ function solution(X, Y) {
     let countX = new Array(10).fill(0);
     let countY = new Array(10).fill(0);
     
-    for(let i of X) {
-        countX[i]++;
+    for(let num of X) {
+        countX[num]++;
     }
     
-    for(let i of Y) {
-        countY[i]++;
+    for(let num of Y) {
+        countY[num]++;
     }
     
     for(let i = 9; i>=0; i--) {
-        let minNum = Math.min(countX[i],countY[i]);
-        
+        let minNum = Math.min(countX[i], countY[i]);
         result = result + String(i).repeat(minNum);
-    }
-    
-    if(result.length === 0) {
-        return "-1";
     }
     
     if(result[0] === "0") {
         return "0";
     }
     
+    if(result.length === 0) {
+        return "-1";
+    }
+    
     return result;
-
 }
