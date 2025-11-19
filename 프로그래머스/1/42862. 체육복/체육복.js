cec6e6931e55dc555965a182ea28d0221ebbc4eb
Sangeok <hamsoo159@naver.com>
2025-11-19 22:51:24 +0900

    [level 1] Title: 체육복, Time: 3.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,21 +1,24 @@
 function solution(n, lost, reserve) {
-    let real_reserve = reserve.filter((item) => !lost.includes(item));
     let real_lost = lost.filter((item) => !reserve.includes(item));
+    let real_reserve = reserve.filter((item) => !lost.includes(item));
     
-    let result = n - real_lost.length;
+    let reserve_set = new Set(real_reserve);
+    let not_lost = n - real_lost.length;
     
     real_lost.sort((a,b) => a-b);
-    let reserve_set = new Set(real_reserve);
     
     for(let lost_student of real_lost) {
-        if(reserve_set.has(lost_student - 1)) {
-            result++;
-            reserve_set.delete(lost_student - 1);
-        } else if(reserve_set.has(lost_student + 1)) {
-            result++;
-            reserve_set.delete(lost_student + 1);
+        if(reserve_set.has(lost_student-1)) {
+            console.log("퍽1")
+            not_lost++;
+            reserve_set.delete(lost_student-1)
+            continue;
+        } else if(reserve_set.has(lost_student+1)) {
+            console.log("퍽2")
+            not_lost++
+            reserve_set.delete(lost_student+1)
         }
     }
     
-    return result;
+    return not_lost;
 }
