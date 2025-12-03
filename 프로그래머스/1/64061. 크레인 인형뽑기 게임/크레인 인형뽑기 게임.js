ae7477c08be7a5d30ac713506f45b37c0864d1fe
Sangeok <hamsoo159@naver.com>
2025-12-04 00:43:18 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 29.22 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -4,22 +4,25 @@ function solution(board, moves) {
     
     for(let move of moves) {
         let currentMove = move - 1;
-        // let currentDoll = 0;
         
         for(let i = 0; i<board.length; i++) {
-            let currentDoll = board[i][currentMove];
-            if(board[i][currentMove] !== 0) {
+            let currentPosition = board[i][currentMove];
+            let findDoll = false;
+            
+            if(currentPosition !== 0) {
+                stack.push(currentPosition);
                 board[i][currentMove] = 0;
-                stack.push(currentDoll);
-                break;
+                findDoll = true;
             }
-        }
-        
-        while(stack.length >= 2 && stack[stack.length-1] === stack[stack.length-2]) {
-            stack.pop();
-            stack.pop();
-            result++;
-            result++;
+            
+            while(stack.length >= 2 && stack[stack.length-1] === stack[stack.length-2]) {
+                stack.pop();
+                stack.pop();
+                result++;
+                result++;
+            }
+            
+            if(findDoll) break;
         }
     }
     
