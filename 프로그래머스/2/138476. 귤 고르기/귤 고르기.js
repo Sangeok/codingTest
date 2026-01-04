d8f8338db2cca5deb3a6b4e22cdecc840375179e
Sangeok <hamsoo159@naver.com>
2026-01-05 07:59:34 +0900

    [level 2] Title: 귤 고르기, Time: 132.93 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,62 +1,35 @@
 function solution(k, tangerine) {
-    // 각 크기별 귤의 개수를 저장하는 객체
-    const countBySize = {};
+    let obj = {}
+    let arr = [];
     
-    // 각 크기별 귤의 개수 계산
-    tangerine.forEach(size => {
-        countBySize[size] = (countBySize[size] || 0) + 1;
-    });
+    let result = 0;
+    let currentCount = 0;
     
-    // 개수를 기준으로 내림차순 정렬
-    const counts = Object.values(countBySize).sort((a, b) => b - a);
+    for(let i of tangerine) {
+        obj[i] = (obj[i] + 1) || 1;
+    }
     
-    // k개를 채울 때까지 귤 선택
-    let total = 0;
-    let types = 0;
+    for(let [key, value] of Object.entries(obj)) {
+        let newArr = [key, value];
+        arr.push(newArr);
+    }
     
-    for (const count of counts) {
-        total += count;
-        types++;
+    arr.sort((a,b) => {
+        return b[1] - a[1];
+    })
+    
+    for(let i = 0; i<arr.length; i++) {
+        currentCount = currentCount + arr[i][1];
+        result = result + 1;
         
-        if (total >= k) {
+        if(currentCount >= k) {
             break;
         }
     }
     
-    return types;
+    return result;
+    
+    
 }
 
-// function solution(k, tangerine) {
-//     let obj = {};
-
-//     for(let i = 0; i<tangerine.length; i++) {
-//         obj[tangerine[i]] = (obj[tangerine[i]] || 0) + 1;
-//     }
-
-//     const objArr = Object.entries(obj);
-//     objArr.sort((a,b) => a[1] - b[1]);
-    
-//     let newArr = [];
-//     for(let i = 0; i<objArr.length; i++) {
-//         for(let j = 0; j<objArr[i][1]; j++) {
-//             newArr.push(objArr[i][0]);
-//         }
-//     }
-
-//     let spliceArr = newArr.splice(newArr.length-k,k);
-    
-//     let set = new Set(spliceArr);
-    
-//     return set.size;
-    
-    
-// }
-
-// k = 6
-// [1,3,2,5,4,5,2,3] => [1,2,2,3,3,4,5,5] => [1,4,3,3,5,5,2,2] = [3,3,5,5,2,2]
-
-// k = 4
-// [5,5,2,2]
-
-// k = 2
-// [3,2,2,2,1,1,1,1] => [1,1,1,1,2,2,2,3]
+// 서로 다른 종류의 귤을 최소화 하고 싶으면 개수가 많은 순서대로 1개의 박스에 넣으면 됨.
