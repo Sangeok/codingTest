2c14babf0c07bfab1a5eac0302451e41135777ba
Sangeok <hamsoo159@naver.com>
2026-01-05 19:03:05 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,16 +1,9 @@
 function solution(n) {
-    if(n === 1) return 1;
-    if(n === 2) return 2;
-    
-    fibo1 = 1;
-    fibo2 = 2;
+    let fib = [0,1,2];
     
     for(let i = 3; i<=n; i++) {
-        let result = (fibo1 + fibo2)%1234567;
-        
-        fibo1 = fibo2;
-        fibo2 = result;
+        fib[i] = (fib[i-1] + fib[i-2]) % 1234567;
     }
     
-    return fibo2;
+    return fib[n];
 }
