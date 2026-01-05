eaaf6cbffe065bab210824fc02a23128d7524d23
Sangeok <hamsoo159@naver.com>
2026-01-05 19:31:16 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,26 +1,31 @@
 function solution(n, words) {
-    let usedword = new Set();
+    let currentPerson = 1;
+    let beforeWord = "";
     
-    usedword.add(words[0]);
+    let wordObj = {};
+    let personObj = {};
     
-    for(let i = 1; i<words.length; i++) {
-        let preWord = words[i-1];
-        let nowWord = words[i];
-        
-        let preWordLastChar = preWord[preWord.length-1];
-        let nowWordFirstChar = nowWord[0];
-        
-        if(usedword.has(nowWord) || preWordLastChar !== nowWordFirstChar) {
-            const nowPersonNum = (i%n)+1;
-            const nowCount = Math.floor(i/n)+1;
-            
-            return [nowPersonNum, nowCount];
-        }
-        
-        usedword.add(words[i]);
+    for(let i = 1; i<=n; i++) {
+        personObj[i] = 1;
     }
     
-    return [0,0]
+    for(let word of words) {
+        wordObj[word] = (wordObj[word] + 1) || 1;
+        
+        // 이미 말한 단어일 경우
+        if(wordObj[word] > 1) {
+            return [currentPerson, personObj[currentPerson]];
+        }
+        
+        // 끝말잇기 규칙 위반인 경우
+        if(beforeWord !== "" && (beforeWord.charAt(beforeWord.length-1) !== word.charAt(0))) {
+          return [currentPerson, personObj[currentPerson]];
+        }
+
+        personObj[currentPerson] = personObj[currentPerson] + 1;
+        currentPerson = ((currentPerson + 1) % (n+1)) || 1
+        beforeWord = word
+    }
     
-    
+    return [0,0];
 }
