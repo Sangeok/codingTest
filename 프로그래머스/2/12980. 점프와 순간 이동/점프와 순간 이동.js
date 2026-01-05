5df4f532bedc03ddd68bad3e34d8f2cc7b520a34
Sangeok <hamsoo159@naver.com>
2026-01-05 09:00:25 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,11 +1,10 @@
-function solution(n)
-{
+function solution(n) {
     let count = 0;
+    
     while(n > 0) {
         if(n % 2 === 0) {
             n = n / 2;
-        }
-        else if(n % 2 === 1) {
+        } else {
             n = n - 1;
             count++;
         }
