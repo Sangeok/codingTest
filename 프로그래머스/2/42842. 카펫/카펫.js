f4d3641b54bcd255903cb010b35712b2daaae9f8
Sangeok <hamsoo159@naver.com>
2026-01-06 19:07:58 +0900

    [level 2] Title: 카펫, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,14 +1,15 @@
 function solution(brown, yellow) {
-    let total = yellow + brown;
+    let total = brown + yellow;
     
     for(let h = 3; h<=Math.sqrt(total); h++) {
         if(total % h === 0) {
             let w = total / h;
             
-            if((w-2) * (h-2) == yellow) {
+            if((w - 2) * (h-2) === yellow) {
                 return [w,h]
             }
         }
     }
-    return []
+    
+    return [0,0]
 }
