6daadf47e26093be3906b34ec74a4eb59529fa02
Sangeok <hamsoo159@naver.com>
2026-01-06 19:27:25 +0900

    [level 2] Title: 예상 대진표, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,11 +1,12 @@
 function solution(n,a,b) {
-    let result = 0;
-    while(a != b) {
-        a = Math.ceil(a / 2);
-        b = Math.ceil(b / 2);
+    let count = 0;
+    
+    while(a !== b) {
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
         
-        result++;
+        count++
     }
     
-    return result;
+    return count;
 }
