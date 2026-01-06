96fc14fa13331642f297cb0ce10b5af203737a58
Sangeok <hamsoo159@naver.com>
2026-01-06 18:58:26 +0900

    [level 2] Title: 피보나치 수, Time: 4.66 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -2,9 +2,9 @@ function solution(n) {
     let fib = [0,1];
     
     for(let i = 2; i<=n; i++) {
-        fib[i] = (fib[i-1] + fib[i-2]) % 1234567;
+        fib[i] = (fib[i-2] + fib[i-1]) % 1234567;
     }
     
-    return fib[n];
+    return fib[n]
 }
 
