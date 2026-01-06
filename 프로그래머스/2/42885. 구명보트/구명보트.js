c929750d2fbc6cb9b75a8fd56c47fd9509b0b98e
Sangeok <hamsoo159@naver.com>
2026-01-06 19:14:37 +0900

    [level 2] Title: 구명보트, Time: 32.34 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -3,19 +3,22 @@ function solution(people, limit) {
     
     let left = 0;
     let right = people.length-1;
+    
     let count = 0;
     
     while(left <= right) {
         if(people[left] + people[right] <= limit) {
-            left++;
+            // left, right 둘 다 태운 경우.
+            left++
+            right--
+            count++
+        } else {
+            // left, right 둘 다 태울 수 없는 경우.
+            // right는 혼자 태워야 함.
+            right--;
+            count++;
         }
-        
-        right--;
-        
-        count++;
     }
     
     return count;
-    
-    
 }
