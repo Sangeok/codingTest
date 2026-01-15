fb4acbec44f0b1b64c00b8ef9d2c68b98829fa89
Sangeok <hamsoo159@naver.com>
2026-01-16 00:56:28 +0900

    [level 2] Title: 할인 행사, Time: 412.62 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,28 +1,31 @@
 function solution(want, number, discount) {
     let obj = {};
+    let result = 0;
+    
     for(let i = 0; i<want.length; i++) {
         obj[want[i]] = number[i];
     }
-
-    let count = 0;
-    let obj2 = {};
-    for(let i = 0; i<discount.length-9; i++) {
-        for(let j = i; j<i+10; j++) {
-            obj2[discount[j]] = (obj2[discount[j]] || 0) + 1;
+    
+    for(let i = 0; i<=discount.length-10; i++) {
+        let obj2 = {};
+        let check = true;
+        for(let k = i; k<i+10; k++) {
+            obj2[discount[k]] = (obj2[discount[k]] + 1) || 1
         }
         
-        let check = true
-        for(let key of Object.keys(obj2)) {
-            if(obj[key] === undefined) check = false;
-            
-            if(obj2[key] > obj[key]) {
+        for(let k = 0; k<want.length; k++) {
+            if(obj[want[k]] !== obj2[want[k]]) {
                 check = false;
+                break;
             }
         }
         
-        if(check) count++;
-        obj2 = {};
+        if(check) result++;
     }
     
-    return count;
+    return result;
 }
+
+// 10일간의 회원 자격
+// 회원일 시 매일 한 가지 제품을 할인함 (할인 제품은 하루에 한 개만 구매 가능)
+// 자신이 원하는 제품과 수량이 할인하는 날짜와 10일 연속으로 일치하는 경우 회원가입
