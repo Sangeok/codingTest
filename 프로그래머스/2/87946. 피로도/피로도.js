a3cc9a3226f4029a082f05271f00ab515453fd6d
Sangeok <hamsoo159@naver.com>
2026-01-18 03:04:58 +0900

    [level 2] Title: 피로도, Time: 34.49 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,26 +1,24 @@
 function solution(k, dungeons) {
-    let result = 0;
+    let n = dungeons.length;
+    let maxCount = 0;
+    let visited = Array(n).fill(false);
     
-    const visited = new Array(dungeons.length).fill(false);
-    
-    function dfs(currentK, count) {
-        result = Math.max(result, count);
+    function DFS(tired, count) {
+        maxCount = Math.max(maxCount, count);
         
-        for(let i = 0; i<dungeons.length; i++) {
-            let [minTired, consumeTired] = dungeons[i];
-            
-            if(!visited[i] && currentK >= minTired) {
+        for(let i = 0; i<n; i++) {
+            if(!visited[i] && tired >= dungeons[i][0]) {
                 visited[i] = true;
-                dfs(currentK - consumeTired, count+1);
+                
+                DFS(tired-dungeons[i][1], count+1);
+                
                 visited[i] = false;
             }
         }
     }
     
-    dfs(k,0);
+    DFS(k,0);
     
-    return result;
+    return maxCount;
+    
 }
-
-// 최소 피로도 : 탐험을 시작하기 위해 필요한 최소 필요도
-// 소모 피로도 : 던전 탐험을 마쳤을 때 소모됨.
