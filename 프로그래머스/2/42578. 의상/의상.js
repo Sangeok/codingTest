f3461ffb161027ecb7d4ac0c2f21a197c1e38c50
Sangeok <hamsoo159@naver.com>
2026-01-17 23:40:54 +0900

    [level 2] Title: 의상, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -5,12 +5,18 @@ function solution(clothes) {
     for(let i = 0; i<clothes.length; i++) {
         let key = clothes[i][1];
         
-        obj[key] = (obj[key]+1) || 1;
+        obj[key] = (obj[key] + 1) || 1
     }
     
-    for(let [key,value] of Object.entries(obj)) {
-        result = result * (value + 1);
+    for(let [key, value] of Object.entries(obj)) {
+        result = result * (value+1);
     }
     
     return result - 1;
+    
+    
 }
+
+// 2 1 => 5 (2+1) X (1+1) - 1;
+
+// 
