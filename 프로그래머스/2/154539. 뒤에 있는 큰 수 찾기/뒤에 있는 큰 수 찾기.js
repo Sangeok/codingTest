1338352aea6724011fc2795514dc0e5af4c4ae2e
Sangeok <hamsoo159@naver.com>
2026-01-19 05:41:12 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 50.83 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,18 +1,15 @@
 function solution(numbers) {
-    let result = Array(numbers.length).fill(-1);
+    let answer = new Array(numbers.length).fill(-1);
     let stack = [];
     
     for(let i = 0; i<numbers.length; i++) {
         while(stack.length > 0 && numbers[stack[stack.length-1]] < numbers[i]) {
-            let idx = stack.pop();
-            result[idx] = numbers[i];
+            let index = stack.pop();
+            answer[index] = numbers[i];
         }
         
         stack.push(i);
     }
     
-    return result;
-    
-    
-    return result;
+    return answer
 }
