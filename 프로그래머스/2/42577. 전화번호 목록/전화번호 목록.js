ce92351eab3c8169699fcb34fb9e3c34b0aebd20
Sangeok <hamsoo159@naver.com>
2026-01-20 21:49:26 +0900

    [level 2] Title: 전화번호 목록, Time: 154.84 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,7 +1,7 @@
 function solution(phone_book) {
-    phone_book.sort()
+    phone_book.sort();
     
-    for(let i = 0; i<phone_book.length-1; i++) {
+    for(let i = 0; i<phone_book.length -1; i++) {
         if(phone_book[i+1].startsWith(phone_book[i])) {
             return false;
         }
