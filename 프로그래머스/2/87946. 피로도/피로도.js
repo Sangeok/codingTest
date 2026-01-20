9d891196922370900f101679794f8fddf99f2131
Sangeok <hamsoo159@naver.com>
2026-01-20 21:37:05 +0900

    [level 2] Title: 피로도, Time: 8.89 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,24 +1,23 @@
 function solution(k, dungeons) {
     let visited = new Array(dungeons.length).fill(false);
-    let max = 0;
+    let result = 0;
     
     DFS(k, 0);
     
-    function DFS(restTired, count) {
-        max = Math.max(max, count);
+    function DFS(currentTired, count) {
+        result = Math.max(result, count);
         
         for(let i = 0; i<dungeons.length; i++) {
             let [needsTired, consumeTired] = dungeons[i];
             
-            if(!visited[i] && needsTired <= restTired) {
+            if(!visited[i] && currentTired >= needsTired) {
                 visited[i] = true;
-                DFS(restTired - consumeTired, count+1);
+                DFS(currentTired - consumeTired, count+1);
                 visited[i] = false;
             }
         }
     }
     
-    return max;
-    
+    return result;
 }
 
