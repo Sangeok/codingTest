27d4d46982a505588618d3899fe2690ac4e70feb
Sangeok <hamsoo159@naver.com>
2026-01-21 00:05:31 +0900

    [level 2] Title: 주식가격, Time: 28.54 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,39 +1,23 @@
-// function solution(prices) {
-//     let stack = [];
-//     let answer = new Array(prices.length).fill(0);
-    
-//     for(let i = 0; i<prices.length; i++) {
-//         while(stack.length && prices[stack[stack.length - 1]] > prices[i]) {
-//             const top = stack.pop();
-//             answer[top] = i-top;
-//         }   
-//         stack.push(i);
-//     }
-    
-//     while(stack.length) {
-//         const pop = stack.pop();
-//         answer[pop] = prices.length - 1 - pop;
-//     }
-    
-//     return answer;
-// }
-
 function solution(prices) {
     let stack = [];
-    let result = new Array(prices.length).fill(0);
+    let answer = new Array(prices.length).fill(0);
+    
     
     for(let i = 0; i<prices.length; i++) {
-        while(stack.length !== 0 && prices[stack[stack.length-1]] > prices[i]) {
-            const top = stack.pop();
-            result[top] = i-top;
+        while(stack.length > 0 && prices[stack[stack.length-1]] > prices[i]) {
+            let pop = stack.pop();
+            answer[pop] = i - pop;
         }
+        
         stack.push(i);
     }
     
     while(stack.length > 0) {
-        let pop = stack.pop();
-        result[pop] = prices.length-1-pop;
+        let shift = stack.shift();
+        let calTime = prices.length - 1 - shift;
+        answer[shift] = calTime;
     }
     
-    return result;
+    return answer;
+    
 }
