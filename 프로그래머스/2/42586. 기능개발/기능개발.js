0c7d6120e120acae4e2acef5313831e8a34d2231
Sangeok <hamsoo159@naver.com>
2026-01-22 02:00:04 +0900

    [level 2] Title: 기능개발, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -4,27 +4,27 @@ function solution(progresses, speeds) {
     for(let i = 0; i<progresses.length; i++) {
         let currentProgress = progresses[i];
         let currentSpeed = speeds[i];
-        let count = 0;
         
+        let calRestDay = 0;
         while(currentProgress < 100) {
             currentProgress = currentProgress + currentSpeed;
-            count++;
+            calRestDay++;
         }
-        restDay.push(count);
+        restDay.push(calRestDay)
     }
     
-    let result = [];
     let maxDay = restDay[0];
     let count = 1;
+    let result = [];
     
     for(let i = 1; i<restDay.length; i++) {
-        if(maxDay < restDay[i]) {
-            maxDay = restDay[i];
-            result.push(count);
-            count = 1;
-        } else {
-            count++;
-        }
+         if(maxDay < restDay[i]) {
+             maxDay = restDay[i];
+             result.push(count);
+             count = 1;
+         } else {
+             count++;
+         }
     }
     
     result.push(count);
