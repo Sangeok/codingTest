a61d97eba29ed889c9616447063238eb6be1d0de
Sangeok <hamsoo159@naver.com>
2026-01-22 02:04:35 +0900

    [level 2] Title: 모음 사전, Time: 19.30 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,17 +1,16 @@
 function solution(word) {
     let dictionary = [];
-    let array = ["A", "E", "I", "O", "U"];
+    let arr = ["A", "E", "I", "O", "U"];
     
-    let str = "";
-    DFS(str, 0);
+    DFS("", 0);
     
     function DFS(currentStr, count) {
         if(count > 5) return;
         
         if(currentStr !== "") dictionary.push(currentStr);
         
-        for(let i = 0; i<array.length; i++) {
-            DFS(currentStr + array[i], count+1);
+        for(let i = 0; i<arr.length; i++) {
+            DFS(currentStr + arr[i], count+1);
         }
     }
     
