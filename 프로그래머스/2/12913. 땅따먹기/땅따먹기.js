bb1c4a293e7a5e37971ef9e549a77dbc73aa9166
Sangeok <hamsoo159@naver.com>
2026-01-24 02:54:52 +0900

    [level 2] Title: 땅따먹기, Time: 46.79 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -1,12 +1,10 @@
 function solution(land) {
-    let n = land.length;
-    
-    for(let i = 1; i< n; i++) {
-        land[i][0] = land[i][0] + Math.max(land[i-1][1], land[i-1][2], land[i-1][3]);
-        land[i][1] = land[i][1] + Math.max(land[i-1][0], land[i-1][2], land[i-1][3]);
-        land[i][2] = land[i][2] + Math.max(land[i-1][0], land[i-1][1], land[i-1][3]);
-        land[i][3] = land[i][3] + Math.max(land[i-1][0], land[i-1][1], land[i-1][2]);
+    for(let i = 1; i<land.length; i++) {
+        land[i][0] = land[i][0] + Math.max(land[i-1][1], land[i-1][2], land[i-1][3])
+        land[i][1] = land[i][1] + Math.max(land[i-1][0], land[i-1][2], land[i-1][3])
+        land[i][2] = land[i][2] + Math.max(land[i-1][0], land[i-1][1], land[i-1][3])
+        land[i][3] = land[i][3] + Math.max(land[i-1][0], land[i-1][1], land[i-1][2])
     }
     
-    return Math.max(land[n-1][0], land[n-1][1], land[n-1][2], land[n-1][3])
+    return Math.max(...land[land.length-1])
 }
