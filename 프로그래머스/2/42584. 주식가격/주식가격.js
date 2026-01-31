ea1bee152a345d4efd3c40cb95a2690fd5b5df40
Sangeok <hamsoo159@naver.com>
2026-01-31 21:57:33 +0900

    [level 2] Title: 주식가격, Time: 29.56 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -2,6 +2,7 @@ function solution(prices) {
     let stack = [];
     let result = new Array(prices.length).fill(0);
     
+    
     for(let i = 0; i<prices.length; i++) {
         while(stack.length > 0 && prices[stack[stack.length-1]] > prices[i]) {
             let index = stack.pop();
@@ -12,9 +13,9 @@ function solution(prices) {
     }
     
     while(stack.length > 0) {
-        let index = stack.pop();
-        let calTime = prices.length - 1 - index;
-        result[index] = calTime;
+        let idx = stack.pop();
+        let calTime = prices.length - 1 - idx;
+        result[idx] = calTime;
     }
     
     return result;
