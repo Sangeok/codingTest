fda19a60c8606cdeefb836259013c1605884f2cf
Sangeok <hamsoo159@naver.com>
2026-02-09 23:47:08 +0900

    [level 2] Title: 숫자 변환하기, Time: 329.59 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,24 +1,24 @@
 function solution(x, y, n) {
     if(x === y) return 0;
     
-    let queue = [[x,0]];
+    const queue = [[x,0]];
     let head = 0;
     
-    let visited = new Array(n+1).fill(false);
-    visited[x] = true;
+    const visited = new Array(y+1).fill(0);
+    visited[x] = 1;
     
     while(head < queue.length) {
-        let [currentValue, count] = queue[head++];
+        const [current, count] = queue[head++];
         
-        let nextStep = [currentValue + n, currentValue * 2, currentValue * 3];
+        let nextStep = [current + n, current * 2, current * 3];
         
         for(let next of nextStep) {
             if(next === y) {
                 return count + 1;
             }
             
-            if(next < y && !visited[next]) {
-                visited[next] = true;
+            if(next < y && visited[next] === 0) {
+                visited[next] = 1;
                 queue.push([next, count+1]);
             }
         }
