cc75dc17be0b73538a98741dc5bf44c63fd71d3e
Sangeok <hamsoo159@naver.com>
2026-02-11 23:52:47 +0900

    [level 2] Title: 소수 찾기, Time: 11.03 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -3,34 +3,34 @@ function solution(numbers) {
     
     let numbersArr = numbers.split('');
     
-    function makePrime(arr, str) {
-        if(str.length > 0) {
-            numberSet.add(Number(str));
-        }
+    function getNum(arr, currentStr) {
+        if(currentStr.length > 0) numberSet.add(Number(currentStr));
         
         if(arr.length === 0) return;
         
         for(let i = 0; i<arr.length; i++) {
-            const copyArr = [...arr];
+            let currentValue = arr[i];
+            let copyArr = [...arr];
             copyArr.splice(i,1);
-            makePrime(copyArr, str + arr[i]);
+            getNum(copyArr, currentStr + currentValue);
         }
     }
     
-    makePrime(numbersArr, "");
+    getNum(numbers, "");
+    
+    console.log("numberSet", numberSet)
     
     return [...numberSet].filter((item) => isPrime(item)).length;
+    
 }
 
 function isPrime(n) {
     if(n < 2) return false;
     if(n === 2) return true;
-    if(n % 2 === 0) return false;
     
-    for(let i = 3; i<=Math.sqrt(n); i++) {
-        if(n % i === 0) {
-            return false;
-        }
+    for(let i = 2; i<=Math.sqrt(n); i++) {
+        if (n % i === 0) return false;
     }
+    
     return true;
 }
