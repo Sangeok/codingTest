d2223a9a288a0bc3ed852630678f474de2caf4a9
Sangeok <hamsoo159@naver.com>
2026-02-12 22:45:14 +0900

    [level 2] Title: 큰 수 만들기, Time: 31.34 ms, Memory: 49.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -0,0 +1,22 @@
+function solution(number, k) {
+    let stack = [];
+    let removeNum = k;
+    
+    for(let i = 0; i<number.length; i++) {
+        let currentNum = number[i];
+        
+        while(stack.length > 0 && removeNum > 0 && stack[stack.length-1] < currentNum) {
+            stack.pop();
+            removeNum--;
+        }
+        
+        stack.push(currentNum);
+    }
+    
+    if (removeNum > 0) {
+        stack.splice(-removeNum);
+    }
+    
+    return stack.join('')
+    
+}
