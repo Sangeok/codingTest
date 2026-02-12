b1d09817504931c09fd1ff43d3251924cc83c8e6
Sangeok <hamsoo159@naver.com>
2026-02-12 16:01:10 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 46.79 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -3,27 +3,25 @@ function solution(queue1, queue2) {
     let sum2 = queue2.reduce((prev, cur) => prev + cur, 0);
     let target = (sum1 + sum2) / 2;
     
-    // 전체 합의 수가 짝수가 아닐 시 애초에 불가능
+    // 두 수의 합이 짝수가 아닌 경우 불가능
     if((sum1 + sum2) % 2 !== 0) return -1;
     
-    const combined = [...queue1, ...queue2];
+    let combined = [...queue1, ...queue2];
     let left = 0;
     let right = queue1.length;
     
-    const limit = queue1.length * 4;
+    let limit = queue1.length * 4;
     let count = 0;
     
     while(count <= limit) {
-        if(sum1 === target) {
-            return count;
-        }
+        if(sum1 === target) return count;
         
-        if(sum1 > target) {
-            sum1 = sum1 - combined[left];
-            left++;
-        } else {
+        if(sum1 < target) {
             sum1 = sum1 + combined[right];
             right++;
+        } else {
+            sum1 = sum1 - combined[left];
+            left++;
         }
         
         count++;
@@ -32,4 +30,5 @@ function solution(queue1, queue2) {
     }
     
     return -1;
+    
 }
