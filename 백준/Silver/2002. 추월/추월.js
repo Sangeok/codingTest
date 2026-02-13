afc0bfa55f3c90bed4edff771bced3c469c9fc56
Sangeok <hamsoo159@naver.com>
2026-02-13 14:35:50 +0900

    [Silver I] Title: 추월, Time: 144 ms, Memory: 9816 KB -BaekjoonHub

--- 백준/Silver/2002. 추월/추월.js
+++ 백준/Silver/2002. 추월/추월.js
@@ -1,28 +1,26 @@
 var input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let N = Number(input.shift());
-let cleanInput = input.map((str) => str.trim());
-
-let inArr = cleanInput.splice(0, N);
-let outArr = cleanInput;
-let result = 0;
+let inArr = input.splice(0, N);
+let outArr = input;
+let count = 0;
 
 let inObj = {};
-
 for (let i = 0; i < inArr.length; i++) {
   inObj[inArr[i]] = i;
 }
 
 for (let i = 0; i < outArr.length; i++) {
-  let currentCarEntryIdx = inObj[outArr[i]];
+  let originValue = inObj[outArr[i]];
 
   for (let j = i + 1; j < outArr.length; j++) {
-    let behindCarEntryIdx = inObj[outArr[j]];
-    if (behindCarEntryIdx < currentCarEntryIdx) {
-      result++;
+    let currentValue = inObj[outArr[j]];
+
+    if (originValue > currentValue) {
+      count++;
       break;
     }
   }
 }
 
-console.log(result);
+console.log(count);
