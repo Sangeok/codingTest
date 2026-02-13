486ed2370df9cb0a6e26de811d562de8a45572e6
Sangeok <hamsoo159@naver.com>
2026-02-13 14:42:06 +0900

    [Silver II] Title: 한 줄로 서기, Time: 84 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.js
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.js
@@ -1,11 +1,11 @@
 var input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let N = Number(input.shift());
-let numArr = input[0].split(" ").map((item) => Number(item));
-
+let arr = input[0].split(" ");
 let result = [];
+
 for (let i = N; i > 0; i--) {
-  let leftSideNum = numArr[i - 1];
+  let leftSideNum = arr[i - 1];
 
   result.splice(leftSideNum, 0, i);
 }
