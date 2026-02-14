f65bfdabe7faf30f0531258473df7b1808ca5aee
Sangeok <hamsoo159@naver.com>
2026-02-14 12:54:57 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 357.33 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -3,36 +3,31 @@ function solution(queue1, queue2) {
     let sum2 = queue2.reduce((prev, cur) => prev + cur, 0);
     let sum = sum1 + sum2;
     
-    let target = (sum / 2);
+    let target = sum / 2;
     
-    // 짝수가 아니라면 불가능.
-    if(sum % 2 !== 0) {
-        return -1;
-    }
+    if(sum % 2 !== 0) return -1;
     
     let newArr = [...queue1, ...queue2];
+    let left = 0;
+    let right = queue1.length;
     
-    let leftIndex = 0;
-    let rightIndex = queue1.length;
+    let limit = queue1.length * 4;
+    let count = 0;
     
-    let result = 0;
-    
-    while(leftIndex < rightIndex) {
-        if(sum1 === target) {
-            return result;
+    while(count <= limit) {
+        if(sum1 === target) return count;
+        
+        if(sum1 > target) {
+            sum1 = sum1 - newArr[left];
+            left++;
+        } else if(sum1 < target) {
+            sum1 = sum1 + newArr[right];
+            right++;
         }
         
-        if(sum1 < target) {
-            sum1 = sum1 + newArr[rightIndex];
-            rightIndex++;
-        } else if(sum1 > target) {
-            sum1 = sum1 - newArr[leftIndex];
-            leftIndex++;
-        }
+        count++;
         
-        result++;
-        
-        if(rightIndex >= newArr.length && sum1 < target) break;
+        if(right > newArr.length && sum1 < target) break;
     }
     
     return -1;
